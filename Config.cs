81016c0c2a6142b7ec54404ca91d6974c359a50a
Claude De-Tchambila <claude.detchambila@dariel.co.za>
2021-11-03 17:38:37 +0200

    Squashed commit of the following:
    commit 5f4052f7d09b4bd32459d9aa01675c3696409376
    Author: Claude De-Tchambila <claude.detchambila@dariel.co.za>
    Date:   Wed Nov 3 17:37:20 2021 +0200
    
        added identity migrations
    
    commit a12ae157b6eeb96191e6c31d2c3f2afa66b3e2c5
    Author: Claude De-Tchambila <claude.detchambila@dariel.co.za>
    Date:   Wed Nov 3 17:17:10 2021 +0200
    
        added sql server to identity server
    
    commit 61718d72e06e217db20a0c5d5e1c5dd3500a42e4
    Author: Claude De-Tchambila <claude.detchambila@dariel.co.za>
    Date:   Mon Nov 1 20:45:16 2021 +0200
    
        Updated client list

--- Config.cs
+++ Config.cs
@@ -4,23 +4,25 @@
 
 using IdentityServer4.Models;
 using System.Collections.Generic;
+using IdentityServer4;
 
 namespace ZanduIdentity
 {
     public static class Config
     {
         public static IEnumerable<IdentityResource> IdentityResources =>
-                   new IdentityResource[]
-                   {
+            new IdentityResource[]
+            {
                 new IdentityResources.OpenId(),
                 new IdentityResources.Profile(),
-                   };
+            };
 
         public static IEnumerable<ApiScope> ApiScopes =>
             new ApiScope[]
             {
                 new ApiScope("scope1"),
                 new ApiScope("scope2"),
+                new ApiScope("Zandu.Core", "Zandu Core API"),
             };
 
         public static IEnumerable<Client> Clients =>
@@ -35,7 +37,7 @@ namespace ZanduIdentity
                     AllowedGrantTypes = GrantTypes.ClientCredentials,
                     ClientSecrets = { new Secret("511536EF-F270-4058-80CA-1C89C192F69A".Sha256()) },
 
-                    AllowedScopes = { "scope1" }
+                    AllowedScopes = { "scope1", "Zandu.Core" }
                 },
 
                 // interactive client using code flow + pkce
@@ -51,8 +53,48 @@ namespace ZanduIdentity
                     PostLogoutRedirectUris = { "https://localhost:44300/signout-callback-oidc" },
 
                     AllowOfflineAccess = true,
-                    AllowedScopes = { "openid", "profile", "scope2" }
+                    AllowedScopes = { "openid", "profile", "scope2", "Zandu.Core" }
                 },
+                // interactive ASP.NET Core MVC client
+                new Client
+                {
+                    ClientId = "zandu.cms.mvc",
+                    ClientSecrets = { new Secret("49C7A7E2-0D79-4A89-A3Z6-A32998FB86B0".Sha256()) },
+
+                    AllowedGrantTypes = GrantTypes.Code,
+
+                    // where to redirect to after login
+                    RedirectUris = { "https://localhost:4001/signin-oidc" },
+
+                    // where to redirect to after logout
+                    PostLogoutRedirectUris =
+                        { "https://localhost:4001/signout-callback-oidc" },
+
+                    AllowedScopes = new List<string>
+                    {
+                        IdentityServerConstants.StandardScopes.OpenId,
+                        IdentityServerConstants.StandardScopes.Profile,
+                        "Zandu.Core"
+                    }
+                },
+                new Client
+                {
+                    ClientId = "zandu.cms",
+                    ClientName = "Zandu CMS Client",
+                    AllowedGrantTypes = GrantTypes.Code,
+                    RequireClientSecret = false,
+
+                    RedirectUris = { "http://localhost:3000/auth/callback" },
+                    PostLogoutRedirectUris = { "http://localhost:3000/auth/logout" },
+                    AllowedCorsOrigins = { "http://localhost:3000" },
+
+                    AllowedScopes = new List<string>
+                    {
+                        IdentityServerConstants.StandardScopes.OpenId,
+                        IdentityServerConstants.StandardScopes.Profile,
+                        "Zandu.Core"
+                    }
+                }
             };
     }
 }
--- Startup.cs
+++ Startup.cs
@@ -2,13 +2,17 @@
 // Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
 
 
+using System.Reflection;
 using IdentityServer4;
+using IdentityServer4.EntityFramework.DbContexts;
+using IdentityServer4.EntityFramework.Mappers;
 using ZanduIdentity.Data;
 using ZanduIdentity.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using System.Linq;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -28,48 +32,23 @@ namespace ZanduIdentity
 
         public void ConfigureServices(IServiceCollection services)
         {
+            var connectionString = Configuration.GetConnectionString("DefaultConnection");
+
+            var migrationsAssembly = typeof(Startup).GetTypeInfo().Assembly.GetName().Name;
+
             services.AddControllersWithViews();
 
-            services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
+            AddASPNetIdentityServices(services, connectionString);
 
-            services.AddIdentity<ApplicationUser, IdentityRole>()
-                .AddEntityFrameworkStores<ApplicationDbContext>()
-                .AddDefaultTokenProviders();
+            AddIdentityServer(services, connectionString, migrationsAssembly);
 
-            var builder = services.AddIdentityServer(options =>
-            {
-                options.Events.RaiseErrorEvents = true;
-                options.Events.RaiseInformationEvents = true;
-                options.Events.RaiseFailureEvents = true;
-                options.Events.RaiseSuccessEvents = true;
-
-                // see https://identityserver4.readthedocs.io/en/latest/topics/resources.html
-                options.EmitStaticAudienceClaim = true;
-            })
-                .AddInMemoryIdentityResources(Config.IdentityResources)
-                .AddInMemoryApiScopes(Config.ApiScopes)
-                .AddInMemoryClients(Config.Clients)
-                .AddAspNetIdentity<ApplicationUser>();
-
-            // not recommended for production - you need to store your key material somewhere secure
-            builder.AddDeveloperSigningCredential();
-
-            services.AddAuthentication()
-                .AddGoogle(options =>
-                {
-                    options.SignInScheme = IdentityServerConstants.ExternalCookieAuthenticationScheme;
-                    
-                    // register your IdentityServer with Google at https://console.developers.google.com
-                    // enable the Google+ API
-                    // set the redirect URI to https://localhost:5001/signin-google
-                    options.ClientId = "copy client ID from Google here";
-                    options.ClientSecret = "copy client secret from Google here";
-                });
+            AddExternalAuthProviders(services);
         }
 
         public void Configure(IApplicationBuilder app)
         {
+            InitializeDatabase(app);
+
             if (Environment.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -81,10 +60,116 @@ namespace ZanduIdentity
             app.UseRouting();
             app.UseIdentityServer();
             app.UseAuthorization();
-            app.UseEndpoints(endpoints =>
+            app.UseEndpoints(endpoints => { endpoints.MapDefaultControllerRoute(); });
+        }
+        
+        private void InitializeDatabase(IApplicationBuilder app)
+        {
+            using (var serviceScope = app.ApplicationServices.GetService<IServiceScopeFactory>().CreateScope())
             {
-                endpoints.MapDefaultControllerRoute();
-            });
+                serviceScope.ServiceProvider.GetRequiredService<PersistedGrantDbContext>().Database.Migrate();
+
+                var context = serviceScope.ServiceProvider.GetRequiredService<ConfigurationDbContext>();
+                context.Database.Migrate();
+                if (!context.Clients.Any())
+                {
+                    foreach (var client in Config.Clients)
+                    {
+                        context.Clients.Add(client.ToEntity());
+                    }
+                    context.SaveChanges();
+                }
+
+                if (!context.IdentityResources.Any())
+                {
+                    foreach (var resource in Config.IdentityResources)
+                    {
+                        context.IdentityResources.Add(resource.ToEntity());
+                    }
+                    context.SaveChanges();
+                }
+
+                if (!context.ApiScopes.Any())
+                {
+                    foreach (var resource in Config.ApiScopes)
+                    {
+                        context.ApiScopes.Add(resource.ToEntity());
+                    }
+                    context.SaveChanges();
+                }
+            }
+        }
+
+        /// <summary>
+        /// Adds the ASP net identity services. This is the persistence framework for storing users and roles.
+        /// </summary>
+        /// <param name="services">The services.</param>
+        /// <param name="connectionString">The connection string.</param>
+        private void AddASPNetIdentityServices(IServiceCollection services, string connectionString)
+        {
+            services.AddDbContext<ApplicationDbContext>(
+                options => options.UseSqlServer(
+                    connectionString));
+
+            services.AddIdentity<ApplicationUser, IdentityRole>(opt =>
+                {
+                    opt.Password.RequireDigit = false;
+                    opt.User.RequireUniqueEmail = true;
+                    opt.SignIn.RequireConfirmedEmail = true;
+                })
+                .AddEntityFrameworkStores<ApplicationDbContext>()
+                .AddClaimsPrincipalFactory<MyUserClaimsPrincipalFactory>()
+                .AddDefaultTokenProviders();
+        }
+
+        private static void AddIdentityServer(IServiceCollection services, string connectionString,
+            string migrationsAssembly)
+        {
+            services.AddIdentityServer(
+                    options =>
+                    {
+                        options.Events.RaiseErrorEvents = true;
+                        options.Events.RaiseFailureEvents = true;
+                        options.Events.RaiseInformationEvents = true;
+                        options.Events.RaiseSuccessEvents = true;
+                        // options.IssuerUri = identityServerOption.IssuerUri;
+                        // options.PublicOrigin = identityServerOption.PublicOrigin;
+                        options.EmitStaticAudienceClaim = true;
+                    })
+                .AddDeveloperSigningCredential()
+                .AddAspNetIdentity<ApplicationUser>()
+                .AddConfigurationStore(
+                    options =>
+                    {
+                        options.ConfigureDbContext = b => b.UseSqlServer(
+                            connectionString,
+                            sql => sql.MigrationsAssembly(migrationsAssembly));
+                    }).AddOperationalStore(
+                    options =>
+                    {
+                        options.ConfigureDbContext = b => b.UseSqlServer(
+                            connectionString,
+                            sql => sql.MigrationsAssembly(migrationsAssembly));
+
+                        // This enables automatic token cleanup.
+                        options.EnableTokenCleanup = true;
+                        options.TokenCleanupInterval = 60;
+                    });
+        }
+
+        private static void AddExternalAuthProviders(IServiceCollection services)
+        {
+            services.AddAuthentication()
+                .AddGoogle(options =>
+                {
+                    options.SignInScheme = IdentityServerConstants.ExternalCookieAuthenticationScheme;
+
+                    // register your IdentityServer with Google at https://console.developers.google.com
+                    // enable the Google+ API
+                    // set the redirect URI to https://localhost:5001/signin-google
+                    options.ClientId = "copy client ID from Google here";
+                    options.ClientSecret = "copy client secret from Google here";
+                });
         }
     }
 }
