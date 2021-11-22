7381f1ead8116a39a99db41e725c53e638d4399f
Chris Claude <christ.tchambila@gmail.com>
2021-11-23 00:50:42 +0200

    added contravariance variable in IRepository for enitty id

--- Data/Repositories/IRepository.cs
+++ Data/Repositories/IRepository.cs
@@ -3,10 +3,10 @@ using System.Threading.Tasks;
 
 namespace ZanduIdentity.Data.Repositories
 {
-    public interface IRepository<T, ID>
+    public interface IRepository<T, in TId>
     {
         Task<IEnumerable<T>> GetAllAsync();
-        Task<T> GetByIdAsync(ID id);
+        Task<T> GetByIdAsync(TId id);
         Task CreateAsync(T e);
         void Update(T user);
         void Delete(T t);
